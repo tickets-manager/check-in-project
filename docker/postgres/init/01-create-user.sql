b3b3e6e2ad99e81754375077227e58e87a10f78c
Vinícius Bittencourt <bittencourt1310@gmail.com>
2023-09-12 00:18:13 -0300

    feat: adiciona backend docker cria o banco postgres e o pgadmin para administração via web

---
+++ docker/postgres/init/01-create-user.sql
@@ -0,0 +1,1 @@
+CREATE USER ticketsuser WITH PASSWORD 'tickets@manager';
---
+++ docker/postgres/init/02-create-databases.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE ticketsmanager;
+ALTER DATABASE ticketsmanager OWNER TO ticketsuser;
+GRANT ALL PRIVILEGES ON DATABASE ticketsmanager TO ticketsuser;
+
+CREATE DATABASE ticketsmanagerhomologation;
+ALTER DATABASE ticketsmanagerhomologation OWNER TO ticketsuser;
+GRANT ALL PRIVILEGES ON DATABASE ticketsmanagerhomologation TO ticketsuser;
