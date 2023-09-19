807d2a7dea8aa410d2da87b1121984e4af5c9e2e
Vinícius Bittencourt <bittencourt1310@gmail.com>
2023-09-18 23:02:21 -0300

    chore: add prisma dependencies

--- docker/postgres/init/01-create-user.sql
+++ docker/postgres/init/01-create-user.sql
@@ -1,1 +1,7 @@
 CREATE USER ticketsuser WITH PASSWORD 'tickets@manager';
+
+-- make prisma user a superuser
+CREATE USER prisma WITH PASSWORD 'prismajs';
+
+ALTER USER prisma WITH SUPERUSER;
+
--- docker/postgres/init/02-create-databases.sql
+++ docker/postgres/init/02-create-databases.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE ticketsmanager;
 ALTER DATABASE ticketsmanager OWNER TO ticketsuser;
 GRANT ALL PRIVILEGES ON DATABASE ticketsmanager TO ticketsuser;
+GRANT ALL PRIVILEGES ON DATABASE ticketsmanager TO prisma;
 
 CREATE DATABASE ticketsmanagerhomologation;
 ALTER DATABASE ticketsmanagerhomologation OWNER TO ticketsuser;
-GRANT ALL PRIVILEGES ON DATABASE ticketsmanagerhomologation TO ticketsuser;
+GRANT ALL PRIVILEGES ON DATABASE ticketstests TO ticketsuser;
+GRANT ALL PRIVILEGES ON DATABASE ticketstests TO prisma;
